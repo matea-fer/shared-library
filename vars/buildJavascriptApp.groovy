c75168d1c07020ca3ce7774667a711098f48665e
Matea Torbarina <matea.torbarina@ericsson.com>
2020-06-05 14:54:34 +0200

    buildJavascriptApp edited

--- vars/buildJavascriptApp.groovy
+++ vars/buildJavascriptApp.groovy
@@ -11,20 +11,6 @@
   }
 } */
 
-def call(Map config=[:], Closure body) {
-    node {
-        git url: "https://github.com/werne2j/sample-nodejs"
-        stage("Install") {
-            sh "npm install"
-        }
-        stage("Test") {
-            sh "npm test"
-        }
-        stage("Deploy") {
-            if (config.deploy) {
-                sh "npm publish"
-            }
-        }
-        body()
-    }
+def call() {
+    sh "echo test"
 }
